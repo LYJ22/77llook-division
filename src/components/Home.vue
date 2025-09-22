6559904e516f1c3ad4ed1e88af5a52a26e05937b
LYJ22 <pippi478@gmail.com>
2025-09-22 19:17:40 +0900

    feat: home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>home</div>
+  <div class="banner-img">
+    <p class="pretendard-jp font-white">ⓒ뵤끼님</p>
+  </div>
+
+  <div id="wrapper">
+    <div style="width: 75%; margin-bottom: 22px">
+      <hr class="top" />
+      <hr class="bottom" />
+      <div class="name">
+        <p class="jp-name pretendard-jp">藤咲凉狐</p>
+        <p class="en-name alfa-slab-one-regular">Iruma Jyuto x Fujisaki Ryoko</p>
+        <p class="jp-name pretendard-jp">入間銃兎</p>
+      </div>
+    </div>
+    <img src="../assets/images/home/badge.png" class="badge" />
+  </div>
+
+  <div class="barcord">
+    <img src="../assets/images/home/barcord.png" />
+    <img src="../assets/images/home/barcord.png" style="right: 0" />
+  </div>
+
+  <div class="bottom-decoration">
+    <div class="handcuff">
+      <img src="../assets/images/home/handcuff.png" />
+    </div>
+    <div class="character-wrapper">
+      <div class="character">
+        <img src="../assets/images/home/character1.png" />
+      </div>
+      <div class="character" style="position: absolute; right: 0">
+        <img src="../assets/images/home/character2.png" />
+      </div>
+    </div>
+  </div>
 </template>
